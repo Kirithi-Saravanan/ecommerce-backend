40419d478f4338a9d9fc329375f05a4942b770dd
Kirithi Saravanan <kirithisaravanan2007@gmail.com>
2025-12-18 09:20:58 +0530

    Added some files

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ createDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "https://ecommerce-frontend1-ten.vercel.app/",
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
